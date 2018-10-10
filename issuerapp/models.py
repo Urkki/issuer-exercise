a69a42869da511cfae05c3d22597275b782c0f62
Urkki <urkeloo@gmail.com>
2018-10-10 23:14:19 +0300

    Added main_currency into Accounts model.  Changed default timezone to Europe/Helsinki.  WIP show_balances & get_ledger_balance functions,

--- issuerapp/models.py
+++ issuerapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models import Q
+from json import dumps
 
 TRANSFER_TYPES = {
     ("credit", "credit"),
@@ -16,7 +18,7 @@ ISSUER_NAME = "issuer"
 #Accounts (holds monetary value in some currency)
 class Accounts(models.Model):
     cardholder = models.CharField(max_length=50, primary_key=True)
-
+    main_currency = models.CharField(max_length=3)
 
     ''' Gets existing account if it exists. Otherwise a new account is created. '''
     @staticmethod
@@ -30,6 +32,9 @@ class Accounts(models.Model):
             new_account.save()
             return new_account
 
+    def __str__(self):
+        return  "{}".format(self.cardholder)
+
 #Transfers (representing a debit or credit)
 class Transfers(models.Model):
     transfer_type = models.CharField(choices=sorted(TRANSFER_TYPES), max_length=6)
@@ -37,26 +42,29 @@ class Transfers(models.Model):
     amount = models.DecimalField(decimal_places=2, max_digits=99)
     account = models.ForeignKey(Accounts, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return "{} {} {} {}".format(self.transfer_type, self.amount,  self.currency, self.account)
+
 #Transactions (bundles a number of transfers to represent movement of value between accounts)
 class Transactions(models.Model):
     transfer_from = models.ForeignKey(Transfers, on_delete=models.CASCADE, related_name="transfer_from")
     transfer_to = models.ForeignKey(Transfers, on_delete=models.CASCADE, related_name="transfer_to")
     transaction_type = models.CharField(choices=sorted(TRANSACTION_TYPES), max_length=13)
-    created = models.DateTimeField("time when transaction was created.")
+    created = models.DateTimeField("time when transaction was created.",default=timezone.now)
 
     @staticmethod
     def create_transaction(debit_account, credit_account, transaction_type, currency, amount):
         debit_transfer = Transfers(transfer_type="debit", currency=currency, amount=amount, account=debit_account)
         credit_transfer = Transfers(transfer_type="credit", currency=currency, amount=amount, account=credit_account)
-        created = timezone.now()
 
         try:
             debit_transfer.save()
             credit_transfer.save()
             #create transaction here because transfers had to be saved before we can reference them.
             transaction = Transactions(transfer_from=debit_transfer, transfer_to=credit_transfer,
-                                       transaction_type=transaction_type, created=created)
+                                       transaction_type=transaction_type)
             transaction.save()
+            return transaction
         except Exception as e:
             #if this fails. delete objects.
             if not debit_transfer.id is None:
@@ -66,3 +74,54 @@ class Transactions(models.Model):
             if not transaction.id is None:
                 transaction.delete()
             raise e
+
+    def __str__(self):
+        return "{0} {1} from: {2} to: {3}"\
+            .format(self.created, self.transaction_type, self.transfer_from, self.transfer_to)
+
+    @staticmethod
+    def show_balances(account_name, time_threshold=timezone.now()):
+        """
+        Calculates ledger balance and available balance for given account.
+        :param account_name: The name of account to get balance
+        :param time_threshold: Time threshold. Balance before this is given.
+        :return: ledger balance and available balance as in JSON format. 
+        """
+        ledger_balance = Transactions.get_ledger_balance(account_name, time_threshold)
+
+    @staticmethod
+    def get_ledger_balance(account_name, time_threshold=timezone.now()):
+        """
+        Calculates ledger balance for given account.
+        :param account_name: The name of account to get ledger balance
+        :param time_threshold: Time threshold. 
+        :return: ledger balance in JSON format. 
+        """
+
+        acc = Accounts.get_account(account_name)
+        transactions_from = Transactions.objects.filter(Q(created__lte=time_threshold),
+                                                        Q(transfer_from__account__exact=acc),
+                                                        Q(transaction_type__exact="presentment"),
+                                                        Q(transfer_from__currency__iexact=acc.main_currency))
+        transactions_to = Transactions.objects.filter(Q(created__lte=time_threshold),
+                                                      Q(transfer_to__account__exact=acc),
+                                                      Q(transaction_type__exact="presentment"),
+                                                      Q(transfer_from__currency__iexact=acc.main_currency))
+        debit_amount = 0
+        credit_amount = 0
+        # debit transfer
+        for transaction in transactions_from:
+            debit_amount += transaction.transfer_from.amount
+        # credit transfer
+        for transaction in transactions_to:
+            credit_amount += transaction.transfer_to.amount
+
+        ledger_balance = credit_amount - debit_amount
+        balance_json = {
+            "ledger_balance": str(ledger_balance)
+        }
+        #print("Ledger debit: {} credit: {} balance: {}".format(debit_amount, credit_amount, ledger_balance))
+        return dumps(balance_json)
+
+
+
